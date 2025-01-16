a71f429fdf7a6342b312e21cabc22a4ab690c1e8
Yevhen <yoshkukov@Yevhens-MBP.localdomain>
2025-01-16 11:17:49 +0100

    Create and run seeder for 10 users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create one standard user
+        User::factory()->create([
+            'name' => 'Admin', // Standard name
+            'email' => 'admin@example.com', // Standard email
+            'password' => bcrypt('password'), // Standard пароль
+        ]);
+    }
+}
