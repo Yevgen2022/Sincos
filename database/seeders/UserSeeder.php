b40fd56ea1d8e8d78fcde2c7d311c55050b7c805
Yevhen <yoshkukov@yevhens-mbp.localdomain>
2025-01-30 16:03:54 +0100

    Create UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,28 +14,52 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
-// Створити роль для адміністратора, якщо вона ще не існує
-        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
-        //$userRole = Role::firstOrCreate(['name' => 'User']);
-
-        // Create one standard user-Admin
+        $adminRole = Role::where('name', 'Admin')->first();
         User::factory()->create([
-            'name' => 'Admin', // Standard name
-            'email' => 'admin@example.com', // Standard email
-            'password' => bcrypt('password'), // Standard пароль
-            'role_id' => $adminRole->id,
+            'name' => 'Admin', // Ім'я адміністратора
+            'email' => 'admin@example.com', // Email адміністратора
+            'password' => bcrypt('password'), // Пароль адміністратора
+            'role_id' => $adminRole->id, // Роль адміністратора
         ]);
 
-        User::factory()->create([
-            'name' => 'Admin Two', // Standard name
-            'email' => 'admin2@example.com', // Standard email
-            'password' => bcrypt('password'), // Standard пароль
-            'role_id' => $adminRole->id,
-        ]);
-
-        // 10 random users
-        User::factory(50)->create();
-
+        // Створення решти користувачів з випадковими ролями (не Admin)
+        $roles = Role::where('name', '!=', 'Admin')->get(); // Всі ролі, окрім Admin
+        User::factory(50)->create()->each(function ($user) use ($roles) {
+            // Випадковим чином присвоюємо одну з ролей для кожного користувача
+            $user->update([
+                'role_id' => $roles->random()->id, // Випадкова роль з масиву
+            ]);
+        });
     }
+
+
+
+
+
+
+
+
+
+
+//// Створити роль для адміністратора, якщо вона ще не існує
+//        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
+//
+//        // Create one standard user-Admin
+//        User::factory()->create([
+//            'name' => 'Admin', // Standard name
+//            'email' => 'admin@example.com', // Standard email
+//            'password' => bcrypt('password'), // Standard пароль
+//            'role_id' => $adminRole->id,
+//        ]);
+//
+//        User::factory()->create([
+//            'name' => 'Admin Two', // Standard name
+//            'email' => 'admin2@example.com', // Standard email
+//            'password' => bcrypt('password'), // Standard пароль
+//            'role_id' => $adminRole->id,
+//        ]);
+//
+//        // 10 random users
+//        User::factory(50)->create();
+//    }
 }
